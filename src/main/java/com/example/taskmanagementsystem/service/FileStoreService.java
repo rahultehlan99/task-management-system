5455eca04b0ab45b9dda0d0b2e9908c4604586a4
Rahul <rtehlan01@gmail.com>
2025-01-09 22:55:29 +0530

    s3 storage service

--- src/main/java/com/example/taskmanagementsystem/service/FileStoreService.java
+++ src/main/java/com/example/taskmanagementsystem/service/FileStoreService.java
@@ -1,11 +1,14 @@
 package com.example.taskmanagementsystem.service;
 
 import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public interface FileStoreService {
-    List<String> upload(List<Resource> file);
+    List<String> upload(List<MultipartFile> file);
 
     List<Resource> download(List<String> file);
+
+    List<String> getObjectPreSignedUrl(List<String> file);
 }
