79337c3300f1839e64b8fd27ce1bf71dda160fdb
Rahul <rtehlan01@gmail.com>
2025-01-09 22:55:03 +0530

    cache config

--- src/main/java/com/example/taskmanagementsystem/configurations/CacheConfig.java
+++ src/main/java/com/example/taskmanagementsystem/configurations/CacheConfig.java
@@ -5,13 +5,16 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @EnableCaching
 @Configuration
 public class CacheConfig {
 
     @Bean("defaultCache")
+    @Primary
     public CacheManager cacheManager(){
+        System.out.println("Cache initialization");
         return new ConcurrentMapCacheManager("taskInfo");
     }
 }
