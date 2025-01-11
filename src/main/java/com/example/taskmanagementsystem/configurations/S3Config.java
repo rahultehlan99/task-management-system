1bb2427328c6e83037a3572bbbc24ea33570f84c
Rahul <rtehlan01@gmail.com>
2025-01-11 12:20:19 +0530

    S3 properties made profile specific

--- src/main/java/com/example/taskmanagementsystem/configurations/S3Config.java
+++ src/main/java/com/example/taskmanagementsystem/configurations/S3Config.java
@@ -5,25 +5,32 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class S3Config {
-
-    // delegate to IAM role for EC2
-    @Value("${aws.s3.access-key}")
-    private String accessKey;
-    @Value("${aws.s3.secret-key}")
-    private String secretKey;
 
     // since S3 is region specific
     @Value("${aws.s3.region}")
     private String region;
 
     @Bean
+    @Primary
     public AmazonS3 s3Client() {
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.fromName(region))
+                .build();
+    }
+
+    @Bean
+    @Profile("dev")
+    public AmazonS3 s3ClientDev(@Value("${aws.s3.access-key}") String accessKey,
+                                @Value("${aws.s3.secret-key}") String secretKey) {
         return AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.fromName(region))
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
